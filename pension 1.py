691fdfc4d020c002fac67b42f08eb0ad54ca160c
Manjunath52 <pallemanjunath52@gmail.com>
2021-04-27 20:43:46 +0530

    changes

---
+++ pension 1.py
@@ -0,0 +1,11 @@
+#program to eligible the old age pension
+x= int(input("Enter x value :"))
+if (x==60):
+ print("this is a required number")
+ print("eligible for old age pension")
+ print("welocme")
+ print("thankyou")
+else :
+ print("not eligible for old age pension")
+ print ("thankyou")
+ print("bye")
